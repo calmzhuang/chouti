cb777be74cea700501f9cfaa6fe405a066a34d22
calmzhuang <QWer1234!@#$>
2018-05-30 20:41:30 +0800

    create a model sample

--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    username = models.CharField(max_length=32, db_index=True)
+    email = models.CharField(max_length=32, unique=True)
+    pwd = models.CharField(max_length=64)
+    cTime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        index_together = [
+            ('username', 'pwd')
+        ]
+
+        unique_together = [
+            ('username', 'pwd')
+        ]
