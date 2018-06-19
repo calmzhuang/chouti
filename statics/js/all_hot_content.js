13ac78ea5503cb64976f0fa3cf784f7e74621bb5
clamzhuang <QWer1234!@#$>
2018-06-19 18:19:32 +0800

    update publish page

--- statics/js/all_hot_content.js
+++ statics/js/all_hot_content.js
@@ -1,15 +1,48 @@
 $(function () {
+   //展示发布页面
    $('#publishBtn').on('click', function () {
        publish_page_show();
    });
+
+   //关闭发布页面
    $('#dialog-btn-close').on('click', function () {
        publish_page_hide();
    });
+
+   //上传并预览图片
    $('#imgUrl').on('change', function () {
        let curent_pic = this.files[0];
-        preview_picture(curent_pic);
-        FileUpload(curent_pic);
+       preview_picture(curent_pic);
+       FileUpload(curent_pic);
+   });
+
+   //发布链接页面
+   $('#pubTabZixun').on('click', function () {
+       let sharetype = 'zixun';
+       let _that = $(this);
+       ShareType(sharetype, _that);
+   });
+
+   //发布文字页面
+   $('#pubTabDuanzi').on('click', function () {
+       let sharetype = 'duanzi';
+       let _that = $(this);
+       ShareType(sharetype, _that);
+   });
+
+   //发布图片页面
+   $('#pubTabPic').on('click', function () {
+       let sharetype = 'tabpic';
+       let _that = $(this);
+       ShareType(sharetype, _that);
+   });
+
+   //
+   $('.toclass-btn').on('click', function () {
+       let _that = $(this);
+       PubIcon(_that);
    })
+
 });
 
 publish_page_show = () => {
@@ -52,4 +85,30 @@ function FileUpload(curent_pic) {
             console.log('ok')
         }
     });
-}
+};
+
+function ShareType(sharetype, _that) {
+    if (sharetype === 'zixun') {
+        _that.addClass('w-active').addClass('color');
+        _that.siblings().removeClass('w-active').removeClass('color');
+        $('#publish-content-zixun').show();
+        $('#publish-content-zixun').siblings().hide();
+    }
+    else if (sharetype === 'duanzi') {
+        _that.addClass('w-active').addClass('color');
+        _that.siblings().removeClass('w-active').removeClass('color');
+        $('#publish-content-duanzi').show();
+        $('#publish-content-duanzi').siblings().hide();
+    }
+    else {
+        _that.addClass('w-active').addClass('color');
+        _that.siblings().removeClass('w-active').removeClass('color');
+        $('#publish-content-pic').show();
+        $('#publish-content-pic').siblings().hide();
+    }
+};
+
+function PubIcon(_that) {
+    _that.addClass('toclass-btn-valid').removeClass('toclass-btn-unvalid');
+    _that.siblings().addClass('toclass-btn-unvalid').removeClass('toclass-btn-valid');
+};
